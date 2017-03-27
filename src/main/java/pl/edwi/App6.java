a11a8c8ef2f4fc8cd36d93b0bb84889ff8259a37
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2017-03-27 20:17:43 +0200

    app6: done.

--- src/main/java/pl/edwi/App6.java
+++ src/main/java/pl/edwi/App6.java
@@ -1,6 +1,5 @@
 package pl.edwi;
 
-
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -19,7 +18,7 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.NIOFSDirectory;
+import org.apache.lucene.store.FSDirectory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.edwi.gut.Book;
@@ -36,15 +35,18 @@ import java.util.regex.Pattern;
 @SuppressWarnings("Duplicates")
 public class App6 {
 
-    public static final String GUT_DVD = "pgdvd042010/";
-    public static final Pattern CARRIAGE_RETURN = Pattern.compile("\r", Pattern.LITERAL);
+    private static final String GUTENBERG_DVD = "pgdvd042010/";
+    private static final String UNZIP_DIRECTORY = GUTENBERG_DVD + "UZ/";
+
+    private static final Pattern FILE_TXT_FORMAT = Pattern.compile("[0-9]+.txt", Pattern.CASE_INSENSITIVE);
+    private static final Pattern FILE_ZIP_FORMAT = Pattern.compile("[0-9]+.zip", Pattern.CASE_INSENSITIVE);
+    private static final Pattern CARRIAGE_RETURN = Pattern.compile("\r", Pattern.LITERAL);
     private static final Pattern WHITESPACES = Pattern.compile("\\s+");
-    private static final Pattern FILENAME_FORMAT = Pattern.compile("[0-9]+.txt");
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final StandardAnalyzer analyzer = new StandardAnalyzer();
-    private final Directory index = new NIOFSDirectory(Paths.get("lucene"));
+    private final Directory index = FSDirectory.open(Paths.get("lucene"));
 
     public App6() throws IOException {
     }
@@ -53,13 +55,13 @@ public class App6 {
 
     public static void main(String[] args) throws IOException {
         App6 app6 = new App6();
-        app6.logger.debug("START");
+        app6.logger.debug("start");
 
-//        app6.unzipAllZips();
-        app6.logger.debug("UNZIP_ALL_ZIPS");
+        app6.unzipAllZips();
+        app6.logger.debug("unzip.done");
 
         app6.parseAllBooks();
-        app6.logger.debug("PARSE_ALL_BOOKS");
+        app6.logger.debug("parse.done");
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -80,30 +82,30 @@ public class App6 {
     // ---------------------------------------------------------------------------------------------------------------
 
     private void unzipAllZips() throws IOException {
+        AtomicInteger counter = new AtomicInteger();
+
         Files.find(
-                Paths.get(GUT_DVD),
+                Paths.get(GUTENBERG_DVD),
                 Integer.MAX_VALUE,
-                (filePath, fileAttr) -> fileAttr.isRegularFile()
+                (path, attr) -> attr.isRegularFile()
+        )
+                .parallel()
+                .filter(path -> FILE_ZIP_FORMAT.matcher(path.getFileName().toString()).matches())
+                .filter(path -> !path.toString().contains("ETEXT"))
+                .filter(path -> !Files.exists(Paths.get(UNZIP_DIRECTORY + path.getFileName() + "-UZ/")))
+                .forEach(path -> {
+                    try {
+                        ZipFile zipFile = new ZipFile(path.toString());
+                        zipFile.extractAll(UNZIP_DIRECTORY + path.getFileName() + "-UZ/");
+                    } catch (ZipException e) {
+                        System.out.printf("unzip.error %s %s%n", path, e);
+                    }
 
-        ).parallel().forEach(zipPath -> {
-            String zipPathStr = zipPath.toString();
-            String zipPathStrLower = zipPathStr.toLowerCase();
-            if (zipPathStrLower.contains("etext") || !zipPathStrLower.endsWith(".zip")) {
-                return;
-            }
-
-            String unpackPathStr = zipPathStr.substring(0, zipPathStr.lastIndexOf('.'));
-            if (Files.exists(Paths.get(unpackPathStr))) {
-                return;
-            }
-
-            try {
-                ZipFile zipFile = new ZipFile(zipPathStr);
-                zipFile.extractAll(unpackPathStr);
-            } catch (ZipException e) {
-                System.out.printf("UNPACK.ERROR %s %s%n", zipPath, e);
-            }
-        });
+                    int cnt = counter.incrementAndGet();
+                    if (cnt % 100 == 0) {
+                        logger.debug("unzip.counter: " + cnt);
+                    }
+                });
     }
 
     // ---------------------------------------------------------------------------------------------------------------
@@ -115,42 +117,40 @@ public class App6 {
         try (IndexWriter indexWriter = new IndexWriter(index, config)) {
 
             Files.find(
-                    Paths.get(GUT_DVD),
+                    Paths.get(UNZIP_DIRECTORY),
                     Integer.MAX_VALUE,
-                    (filePath, fileAttr) -> fileAttr.isRegularFile()
+                    (path, attr) -> attr.isRegularFile()
 
-            ).parallel().forEach(bookPath -> {
-                String filename = bookPath.getFileName().toString();
-                if (!FILENAME_FORMAT.matcher(filename).matches()) {
-                    return;
-                }
+            )
+                    .parallel()
+                    .filter(path -> FILE_TXT_FORMAT.matcher(path.getFileName().toString()).matches())
+                    .forEach(path -> {
+                        try {
+                            byte[] bytes = Files.readAllBytes(path);
+                            String string = new String(bytes, StandardCharsets.ISO_8859_1);
+                            Book book = parseBook(path.getFileName().toString(), string);
 
-                try {
-                    byte[] bytes = Files.readAllBytes(bookPath);
-                    String string = new String(bytes, StandardCharsets.ISO_8859_1);
-                    Book book = parseBook(filename, string);
+                            Document doc = new Document();
+                            doc.add(new StringField("id", book.getId(), Field.Store.YES));
+                            doc.add(new TextField("title", book.getTitle(), Field.Store.YES));
+                            doc.add(new TextField("content", book.getContent(), Field.Store.YES));
+                            indexWriter.addDocument(doc);
 
-                    Document doc = new Document();
-                    doc.add(new StringField("id", book.getId(), Field.Store.YES));
-                    doc.add(new TextField("title", book.getTitle(), Field.Store.YES));
-                    doc.add(new TextField("content", book.getContent(), Field.Store.YES));
-                    indexWriter.addDocument(doc);
+                        } catch (IOException e) {
+                            logger.error("parse.error " + path + ' ' + e);
+                        }
 
-                } catch (IOException e) {
-                    System.out.printf("PARSE.ERROR %s %s%n", bookPath, e);
-                }
-
-                int cnt = counter.incrementAndGet();
-                if (cnt % 100 == 0) {
-                    logger.debug("counter: " + cnt);
-                }
-            });
+                        int cnt = counter.incrementAndGet();
+                        if (cnt % 100 == 0) {
+                            logger.debug("parse.counter: " + cnt);
+                        }
+                    });
         }
     }
 
     // ---------------------------------------------------------------------------------------------------------------
 
-    private Book parseBook(String path, String raw) {
+    private Book parseBook(String filename, String raw) {
         String[] text = CARRIAGE_RETURN.matcher(raw).replaceAll("").split("\n");
 
         int titleIndex = 0;
@@ -171,10 +171,18 @@ public class App6 {
             }
         }
 
-        String title = WHITESPACES.matcher(titleBuilder).replaceAll(" ");
-        String content = String.join("\n", Arrays.asList(text).subList(titleIndex, text.length)).trim();
+        int contentIndex = 0;
+        for (int i = titleIndex; i < text.length; i++) {
+            if (text[i].startsWith("*** START OF THIS PROJECT GUTENBERG")) {
+                contentIndex = i + 1;
+                break;
+            }
+        }
 
-        return new Book(path, title, content);
+        String title = WHITESPACES.matcher(titleBuilder).replaceAll(" ");
+        String content = String.join("\n", Arrays.asList(text).subList(contentIndex, text.length)).trim();
+
+        return new Book(filename, title, content);
     }
 
 
